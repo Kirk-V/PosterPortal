90266f38f231295df20b3f0572345f9bc5bb4e98
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-04-12 15:27:42 -0400

    removed seeding testing code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,46 +24,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        error_log('Some message here.');
+        // error_log('Some message here.');
         // Posters::factory(10)->create();
         // Transactions::factory(10)->create();
         // Courses::factory(10)->create();
         // Requests::factory(10)->create();
 
-        $poster = Models\Posters::factory()
-                ->has(Models\Requests::factory()) //course_id = 1
-                ->has(Models\Jobs::factory()) //course_id = 2
-                ->has(Models\Transactions::factory())
-                ->create()
-                ->first();
-
-        $course = Models\Courses::factory()  //will be id = 3
-        ->has(Models\Transactions::factory(1)) //id=2 has course_id = 3
-        ->has(Models\Requests::factory(1)) //id=2 has course_id = 3
-        ->create()
-        ->first();
-
-        $transaction = Models\Transactions::factory()
-            ->for($course)
-            ->for($poster)
-            ->create()
-            ->first();
-        
-
-        //Add a request;
-        $request = Models\Requests::factory()
-            ->for($poster)
-            ->for($course)
-            ->create()
-            ->first();
-
-
-
-    
-
-        $setting = Models\Settings::factory()
-            ->create()
-            ->first();
+       
 
     }
 }
