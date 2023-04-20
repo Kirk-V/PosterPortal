e55589667f67f115d0b8f7f3b38af338e07eb955
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-04-20 15:34:26 -0400

    color code rows to red if ready for print

--- resources/js/Components/RequestTableRow.jsx
+++ resources/js/Components/RequestTableRow.jsx
@@ -1,9 +1,14 @@
 export default function RequestTableRow({ rowData, headers, handleClick }) {
     console.log(`rowdata: ${JSON.stringify(rowData)}`);
 
-
+    function rowColor(){
+        if(rowData.requests.payment_method == 'speedcode' && rowData.speed_code_approved == '1')
+        {
+            return 'table-danger'
+        }
+    }
     return (
-        <tr onClick={() => handleClick(rowData)}>
+        <tr onClick={() => handleClick(rowData)} className={rowColor()}>
             {Object.values(headers).map((cell, key) => <td key={key}>{eval(`rowData.${cell}`)}</td>)}
         </tr>
     );
