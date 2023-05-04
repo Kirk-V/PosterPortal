d5e30a2623ffceab7776ec0b8f54eaef67cb76d6
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-05-04 13:45:54 -0400

    poster portal renders by default now

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,13 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('posterportal');
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
 });
 
 Route::get('/h', function () {
