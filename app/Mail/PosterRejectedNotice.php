46577a94c0aa61a7b68da1ee8f557a24fa681af1
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-07-03 16:13:26 -0400

    started email notification for accept/reject

---
+++ app/Mail/PosterRejectedNotice.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Posters;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class PosterRejectedNotice extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Posters $poster;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public string $poster_id)
+    {
+                //retrieve the poster data
+       try{
+        $this->poster = Posters::findOrFail($poster_id);
+        }
+        catch(ModelNotFoundException $e)
+        {
+            Log::info("Could not find poster model $e");
+            Mail::to("kvande85@uwo.ca")->send(new SSTSErrorNotification("Error Sending Poster Accepted for printing notice. Could not find poster $poster_id in database $e"));
+        }
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Poster Rejected Notice',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.PosterRejected',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/mail/PosterRejected.blade.php
@@ -0,0 +1,3 @@
+<p>This is an automated message from SSTS printing services</p>
+<p>Your poster is not eligible for printing at this time. For information on eligiblity please check out our <a href="https://ssts.uwo.ca/services/posters/poster_printing_faqs.html">FAQs page.</a></p>
+<p>If you believe this to be a mistake please contact us at <a href="mailto:ssts-posters@uw.ca">SSTS-Posters@uwo.ca</a>
