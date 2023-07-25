f12d7b23fbaa46c064901b2f41d0fb33b6cf2115
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-07-25 14:34:41 -0400

    Page names show as title

--- resources/js/Layouts/PrimaryLayout.jsx
+++ resources/js/Layouts/PrimaryLayout.jsx
@@ -1,10 +1,12 @@
 // import { Link } from '@inertiajs/react';
 import Container from 'react-bootstrap/Container';
 import NavBar from '@/Components/NavBar';
+import {Head} from '@inertiajs/react'
 
 export default function Primary({ onPage, children, signedInUser }) {
     return (     
         <>
+        <Head title={onPage} />
         <NavBar currentView={onPage} user={signedInUser}>
 
         </NavBar>
