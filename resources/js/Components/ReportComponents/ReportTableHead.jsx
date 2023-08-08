4c207265ab6527ede999407ef6173d9e76d8127b
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-08-08 11:35:17 -0400

    fixed column headers

--- resources/js/Components/ReportComponents/ReportTableHead.jsx
+++ resources/js/Components/ReportComponents/ReportTableHead.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 export default function ReportTableHead() {
   return (
     <thead>
+      <tr>
+          <th className="text-center border-end border-top border-bottom-0" colSpan={4}>Information</th>
+          <th className="text-center border-bottom-0 border-top" colSpan={4}>Payment</th>
+        </tr>
         <tr>
           <th>Date</th>
           <th>Poster #</th>
           <th>Requisitioner</th>
-          <th>Approver</th>
-          <th>Grant Payment</th>
-          <th>Cash Payment</th>
-          <th>Total</th>
+          <th className="border-end">Approver</th>
+          <th>Grant</th>
+          <th>Cash</th>
           <th>SDF Discount</th>
-          <th>Received</th>
+          <th>Total</th>
         </tr>
     </thead>
   )
