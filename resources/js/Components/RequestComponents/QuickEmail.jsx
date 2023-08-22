6783b3e8fa36a6713651c5e48c261e5fdb962645
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-08-22 16:25:11 -0400

    starting quick email form

--- resources/js/Components/RequestComponents/QuickEmail.jsx
+++ resources/js/Components/RequestComponents/QuickEmail.jsx
@@ -4,13 +4,41 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 
-export default function QuickEmail({reqEmail}) {
-  console.log("Email "+reqEmail);
+export default function QuickEmail({ reqEmail }) {
+  console.log("Email " + reqEmail);
   return (
     <Row>
-        <Col className="justify-content-end d-flex">
-          <h3><a href={"mailto:"+reqEmail}>send</a></h3>
-        </Col>
+      <Col xs={12} className="bg-primary opacity-50 rounded p-3">
+        <Row>
+          <Col xs={6} >
+          <Form.Group
+            className="mb-3"
+            controlId="requestFormFirstName"
+          >
+            <Form.Control
+              name="first_name"
+              type="text"
+              defaultValue={reqEmail}
+              required
+            />
+          </Form.Group></Col>
+        </Row>
+        <Row>
+          <Col xs={12} >
+          <Form.Group
+            className="mb-3"
+            controlId="requestFormFirstName"
+          >
+            <Form.Control
+              name="first_name"
+              as="textarea"
+              value="test"
+
+              required
+            />
+          </Form.Group></Col>
+        </Row>
+      </Col>
     </Row>
   )
 }
