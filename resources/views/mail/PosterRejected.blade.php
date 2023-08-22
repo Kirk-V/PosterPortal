4320cc14eeec15ddda12840073181aabebb86206
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-08-22 16:11:04 -0400

    email updates

--- resources/views/mail/PosterRejected.blade.php
+++ resources/views/mail/PosterRejected.blade.php
@@ -1,3 +1,12 @@
-<p>This is an automated message from SSTS printing services</p>
-<p>Your poster is not eligible for printing at this time. For information on eligiblity please check out our <a href="https://ssts.uwo.ca/services/posters/poster_printing_faqs.html">FAQs page.</a></p>
-<p>If you believe this to be a mistake please contact us at <a href="mailto:ssts-posters@uw.ca">SSTS-Posters@uwo.ca</a>
+<p>This is an automated message from Social Science Technology Services.</p>
+
+<p>Re: Poster #{{$poster->poster_id}} Submitted by {{$poster->requests->email}}</p>
+<p>This poster printing request has been deleted for one of the following reasons:<br/>
+1.	The applicant has requested to withdraw the poster.<br/>
+2.	The applicant has requested to change the payment method and re-submit the poster. <br/>
+3.	The applicant is not eligible to use the Social Science printing service.<br/>
+4.	The Speedcode approval was denied by the account approver and the applicant does not want to proceed with a cash payment.</p>
+
+<p>If the Speedcode payment option was selected, an automated email will be sent to the account approver that a Speedcode is no longer required. </p>
+
+<p>If you have any questions or concerns, please email <a href="mailto:ssts-posters@uw.ca">SSTS-Posters@uwo.ca</a></p>
