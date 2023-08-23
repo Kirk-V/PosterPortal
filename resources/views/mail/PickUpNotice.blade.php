02beed6a3d21fdc2681eca7c220587372d378a70
Kirk Vander Ploeg <kvande85@uwo.ca>
2023-08-23 12:07:40 -0400

    small change on pick up email

--- resources/views/mail/PickUpNotice.blade.php
+++ resources/views/mail/PickUpNotice.blade.php
@@ -2,7 +2,7 @@
 <p>Your poster is ready to pick up from Social Science Room 1226 (8:30 to 4:30 M-F). <br>
     Total cost of the poster is ${{number_format($Cost, 2)}}<br/>
     Payment is by Cash (NO Credit cards, Debit cards or cheques) or Speedcode (as per the payment method entered on the form).
-    Posters will be packaged in a clear plastic poster bag for weather protection.<br><br>
+    <br/>Posters will be packaged in a clear plastic poster bag for weather protection.<br><br>
     Thank You!
 </p>
 <p>
